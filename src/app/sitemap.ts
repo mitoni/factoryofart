677a2f6d6660a2924b4120e667fd694e20a2ecd2
michele-tonizzo <mt@moi.am>
2024-06-13 08:48:32 -0400

    fix url in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,13 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `https://${process.env.VERCEL_URL}`,
+      url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/about-us`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
